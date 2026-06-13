92f08c80d30cefe65459ea74f4071696d773f05e
Oliver Coates <oliver.gcoates@gmail.com>
2026-06-13 15:51:19 +1200

    Added some useful links I need to read

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 
+// To read:
+// Avalonia UI - https://docs.avaloniaui.net/docs/get-started/create-your-first-project
+// MVVM Architecture - https://learn.microsoft.com/en-us/dotnet/architecture/maui/mvvm
+// Processing XML - https://learn.microsoft.com/en-us/dotnet/standard/data/xml/xml-processing-options
+// PDF procesing - https://github.com/QuestPDF/QuestPDF
+// Deploying to executable - https://learn.microsoft.com/en-us/dotnet/core/deploying/single-file/overview?tabs=cli
+
+
 namespace Deer_O_Matic
 {
     class Program
